9a235ddce7311371b7bc444166160ad1d2b2e6f9
emrahaydemir <firstlord97@gmail.com>
2024-09-06 17:29:33 +0300

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+from dotenv import load_dotenv
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4")
+
+
+search = TavilySearchResults(max_results=2)
+
+tools = [search]
+
+model_with_tools = model.bind_tools(tools)
+
+if __name__ == "__main__":
+    print("Hello agent")
