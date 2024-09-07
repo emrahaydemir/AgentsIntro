3ea4eeb812860defb7d85fd4407bba706dbac72b
emrahaydemir <firstlord97@gmail.com>
2024-09-07 10:51:53 +0300

    rag memory

--- main.py
+++ main.py
@@ -3,16 +3,42 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 
+# from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.sqlite import SqliteSaver
+from langchain import hub
+from langchain.agents import create_react_agent, AgentExecutor
+
 load_dotenv()
 
 model = ChatOpenAI(model="gpt-4")
 
+memory = SqliteSaver.from_conn_string(":memory:")
 
 search = TavilySearchResults(max_results=2)
 
+prompt = hub.pull("hwchase17/react")
+
+
 tools = [search]
 
-model_with_tools = model.bind_tools(tools)
+agent = create_react_agent(model, tools, prompt)
+
+agent_executor = AgentExecutor(
+    agent=agent, tools=tools, verbose=True, checkpoint=memory
+)
+
+config = {"configurable": {"thread_id": "abc123"}}
 
 if __name__ == "__main__":
-    print("Hello agent")
+    chat_history = []
+    while True:
+        user_input = input("> ")
+        response = []
+        for chunk in agent_executor.stream(
+            {"input": user_input, "chat_history": "\n".join(chat_history)},
+            config=config,
+        ):
+            if 'text' in chunk:
+                print(chunk["text"], end="")
+                response.append(chunk["text"])
+        chat_history.append(f"AI: {''.join(response)}")
